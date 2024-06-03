0d35ce07bc4a0a241cb4aa01c7126b355f39f5c7
rnsp202 <165612437+rnsp202@users.noreply.github.com>
2024-06-03 18:07:39 +0900

    casper

---
+++ scripts/casper.js
@@ -0,0 +1,18 @@
+//변수
+const sub = document.querySelectorAll('.sub')
+const sub_bg = document.querySelector('.sub_bg')
+const nav = document.querySelector('nav')
+console.log(sub)
+console.log(sub_bg)
+console.log(nav)
+const nav_clone = nav.cloneNode(true)
+console.log(nav_clone)
+const m_nav = document.querySelector('.m_nav')
+console.log(m_nav) //복제대상 붙여넣기 용
+
+//모든 서브 숨기기
+sub_bg.style.display = 'none'
+for(let i of sub){i.style.display = 'none'}
+
+//m_nav 모바일 전용 메뉴의 기존 내비게이션 태그 마지막 자식 복붙
+m_nav.appendChild(nav_clone)
